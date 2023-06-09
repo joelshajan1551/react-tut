cae815f8330f7c9a549a5088b3b26e49354b85ae
joelshajan1551 <joelshajan0000@gmail.com>
2023-06-09 10:05:49 +0530

    added books

--- src/index.js
+++ src/index.js
@@ -2,34 +2,28 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import "./index.css";
 
+const Author = "James clear";
+const title = " Atomic Habits";
+const img =
+  "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg";
 const Booklist = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book job="dev" />
+      <Book title="random titl" number={21} />
     </section>
   );
 };
-
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{Author}</h4>
+      {console.log(props)}
     </article>
   );
-};
-
-const Image = () => (
-  <img src="https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg" />
-);
-const Title = () => <h2>Atomic Habits</h2>;
-const Author = () => {
-  return <h4> James Clear </h4>;
 };
 
 const root = ReactDom.createRoot(document.getElementById("root"));
